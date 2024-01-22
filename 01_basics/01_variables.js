92cce45a1f5b963ef73551806027676e32f328bb
anandyadav5ster <36939602+anandyadav5ster@users.noreply.github.com>
2024-01-22 04:38:11 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail = 'test@gmail.com'
+var accountPassword ='12345'
+
+// const reassigment not allowed
+// accountId = 2
+
+accountEmail = 'test@hotmail.com'
+accountPassword = '1357'
+
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all js code as newer version
+
+let city_name= "bangalore"
+let age = 40
+let islogin = true
+
+console.log(typeof(city_name));
+console.log(typeof age)
+console.log(typeof islogin);
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
